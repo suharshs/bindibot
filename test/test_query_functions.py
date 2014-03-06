4516cfabf68276c101d4caf85e8165b9a0ff7db7
suharshs <suharshs@gmail.com>
2014-03-05 21:17:46 -0600

    added tag query test

--- test/test_query_functions.py
+++ test/test_query_functions.py
@@ -25,6 +25,21 @@ class TestQueryFunctions(unittest.TestCase):
       json.loads(query_functions.default_match_query(question_doc)),
       json.loads(query_string))
 
+  def test_tag_default_query(self):
+    """
+    Test the tag_default_query function.
+    """
+    question_doc = {}
+    question_doc['question'] = 'question'
+    question_doc['subject'] = 'subject'
+    question_doc['tags'] = 'tag1 tag2'
+    query_string = ('{"query":{"bool":{"should":[{"match":{"question":' + 
+                    '"question"}},{"match":{"tags":{"query":"tag1 tag2",' +
+                    '"boost":2}}},{"match":{"subject":"subject"}}]}}}')
+    self.assertEqual(
+      json.loads(query_functions.tag_default_query(question_doc)),
+      json.loads(query_string))
+
 
 if __name__ == '__main__':
   unittest.main()
