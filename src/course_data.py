b7fe7d477ae7a9bd9fda864b1757a63d99a18a53
suharshs <suharshs@gmail.com>
2014-03-10 16:41:16 -0500

    separated course_data from populate_data script

---
+++ src/course_data.py
@@ -0,0 +1,43 @@
+"""
+Contains data for cs courses.
+"""
+
+courses = {
+  'cs225': [
+    {
+      'name': "fa13",
+      'course_id': "hgqm4pisrhx5a",
+      'max_id': 4100
+    },
+    {
+      'name': "sp13",
+      'course_id': "haj1ly2qdsv1xb",
+      'max_id': 2700
+    },
+    {
+      'name': "fa12",
+      'course_id': "h5oj0u0mpctyq",
+      'max_id': 3200
+    },
+    {
+      'name': "sp12",
+      'course_id': "gx9jljivqar210",
+      'max_id': 2900,
+      'tag_map': {
+        'lab1': 'lab_intro',
+        'lab2': 'lab_debug',
+        'lab3': 'lab_memory',
+        'lab4': 'lab_inheritance',
+        'lab5': 'lab_parallel',
+        'lab6': 'lab_race',
+        'lab7': 'lab_quacks',
+        'lab8': 'lab_trees',
+        'lab9': '',
+        'lab10': 'lab_hash',
+        'lab11': 'lab_reduce',
+        'lab12': 'lab_heaps',
+        'lab13': 'lab_graphs',
+      }
+    }
+  ]
+}
--- src/populate_data.py
+++ src/populate_data.py
@@ -10,29 +10,7 @@ Gets all cs225 previous semester data and populate elasticsearch index.
 import argparse
 from piazza_api import PiazzaAPI
 
-
-courses = [
-    {
-      'name': "fa13",
-      'course_id': "hgqm4pisrhx5a",
-      'max_id': 4100
-    },
-    {
-      'name': "sp13",
-      'course_id': "haj1ly2qdsv1xb",
-      'max_id': 2700
-    },
-    {
-      'name': "fa12",
-      'course_id': "h5oj0u0mpctyq",
-      'max_id': 3200
-    },
-    {
-      'name': "sp12",
-      'course_id': "gx9jljivqar210",
-      'max_id': 2900
-    }
-]
+from course_data import courses
 
 
 if __name__ == '__main__':
@@ -52,9 +30,11 @@ if __name__ == '__main__':
                       required=True)
   args = parser.parse_args()
 
+  course = courses['cs225']
+
   piazza_api = PiazzaAPI(args.username, args.password)
   for course in courses:
     piazza_api.write_course_data_elasticsearch(
         es_hosts=args.elasticsearch_hosts, es_index=args.elasticsearch_index,
         es_type=args.elasticsearch_type, course_id=course['course_id'],
-        start_id=1, end_id=course['max_id'])
+        start_id=1, end_id=course['max_id'], tag_map=course['tag_map'])
