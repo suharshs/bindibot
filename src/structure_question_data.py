ccfa5825e31d3a9cf17c9af31a1e7aa5ba42e281
suharshs <suharshs@gmail.com>
2014-03-13 18:11:59 -0500

    structure question data now splits questions and answers to one to one correspondance so that bindibot can only supply one answer

--- src/structure_question_data.py
+++ src/structure_question_data.py
@@ -10,6 +10,7 @@ a more useful structure for bindibot.
 """
 
 import argparse
+from copy import deepcopy
 import elasticsearch
 import course_data
 
@@ -77,6 +78,45 @@ def get_structured_doc(raw_doc):
       structured_doc['followups'].append(followup_doc)
   return structured_doc
 
+def get_structured_docs(raw_doc):
+  """
+  Given a raw json object will return a list of structured json objects.
+  One for each type of answer.
+  """
+  docs = []
+  structured_doc = {}
+  structured_doc['question'] = raw_doc['result']['history'][0]['content']
+  structured_doc['question_upvotes'] = len(raw_doc['result']['tag_good_arr'])
+  structured_doc['subject'] = raw_doc['result']['history'][0]['subject']
+  structured_doc['answer'] = ''
+  structured_doc['cid'] = raw_doc['result']['id']
+  tag_map = course_data.tag_maps[raw_doc.get('course_name', 'default')]
+  for i in range(len(raw_doc['result']['tags'])):
+    if raw_doc['result']['tags'][i] in tag_map:
+      raw_doc['result']['tags'][i] = tag_map[raw_doc['result']['tags'][i]]
+  structured_doc['tags'] = ' '.join(raw_doc['result']['tags'])
+  structured_doc['followups'] = []
+  for child in raw_doc['result']['children']:
+    if child['type'] == 'followup':
+      followup_doc = {}
+      followup_doc['uid'] = child.get('uid', 'ANON')
+      followup_doc['content'] = child['subject']
+      followup_doc['comments'] = []
+      for comment in child['children']:
+        followup_doc['comments'].append({
+          'uid': comment.get('uid', 'ANON'),
+          'content': comment['subject']
+        })
+      structured_doc['followups'].append(followup_doc)
+  for child in raw_doc['result']['children']:
+    if ((child['type'] == 's_answer' or child['type'] == 'i_answer')
+        and len(child['history']) > 0):
+      structured_doc['answer'] = child['history'][0]['content']
+      structured_doc['answer_upvotes'] = len(child['tag_endorse'])
+      structured_doc['type'] = child['type']
+      docs.append(deepcopy(structured_doc))
+  return docs
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Structure raw json data.')
@@ -102,7 +142,8 @@ if __name__ == '__main__':
   dest_es = elasticsearch.Elasticsearch(args.es_dest_hosts)
   current_doc = source_iterator.next()
   while current_doc is not None:
-    structured_doc = get_structured_doc(current_doc)
-    if structured_doc['i_answer'] or structured_doc['s_answer']:
-      dest_es.index(args.es_dest_index, args.es_dest_type, body=structured_doc)
+    structured_docs = get_structured_docs(current_doc)
+    for structured_doc in structured_docs:
+      if structured_doc['answer']:
+        dest_es.index(args.es_dest_index, args.es_dest_type, body=structured_doc)
     current_doc = source_iterator.next()
