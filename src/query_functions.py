7c7effb6a67bb91fc112b1d0c147dadf4ebd95f2
suharshs <suharshs@gmail.com>
2014-03-30 19:06:22 -0500

    added various boosts for comparison in tag matching query functions

--- src/query_functions.py
+++ src/query_functions.py
@@ -1,9 +1,6 @@
 """
 Query functions to be passed to top_answers module.
 Each query will score results differently.
-Possible query functions are:
-'default_match_query'
-'tag_default_query'
 """
 
 import json
@@ -30,9 +27,62 @@ def default_match_query(question_doc):
     query_dict['query']['bool']['should'].append({'match':{'subject':subject}})
   return json.dumps(query_dict)
 
-def tag_default_query(question_doc):
+def tag_match_query_1(question_doc):
   """
-  Returns the query string to search for related posts with tag limitation.
+  Returns the query string to search for related posts with tag limitation with
+  a boost of 1 on the tag.
+  """
+  question = question_doc['question'][:1000] # limit query length
+  subject = question_doc['subject']
+  tags = question_doc['tags']
+  query_dict = {
+    'query': {
+      'bool': {
+        'should': [
+          {
+            'match' : {'question' : question}
+          },
+          {
+            'match' : {'tags': {'query':tags, 'boost':1}}
+          }
+        ]
+      }
+    }
+  }
+  if subject:
+    query_dict['query']['bool']['should'].append({'match':{'subject':subject}})
+  return json.dumps(query_dict)
+
+def tag_match_query_2(question_doc):
+  """
+  Returns the query string to search for related posts with tag limitation with
+  a boost of 2 on the tag.
+  """
+  question = question_doc['question'][:1000] # limit query length
+  subject = question_doc['subject']
+  tags = question_doc['tags']
+  query_dict = {
+    'query': {
+      'bool': {
+        'should': [
+          {
+            'match' : {'question' : question}
+          },
+          {
+            'match' : {'tags': {'query':tags, 'boost':2}}
+          }
+        ]
+      }
+    }
+  }
+  if subject:
+    query_dict['query']['bool']['should'].append({'match':{'subject':subject}})
+  return json.dumps(query_dict)
+
+def tag_match_query_3(question_doc):
+  """
+  Returns the query string to search for related posts with tag limitation with
+  a boost of 3 on the tag.
   """
   question = question_doc['question'][:1000] # limit query length
   subject = question_doc['subject']
@@ -55,9 +105,38 @@ def tag_default_query(question_doc):
     query_dict['query']['bool']['should'].append({'match':{'subject':subject}})
   return json.dumps(query_dict)
 
+def tag_match_query_100(question_doc):
+  """
+  Returns the query string to search for related posts with tag limitation with
+  a boost of 100 on the tag.
+  """
+  question = question_doc['question'][:1000] # limit query length
+  subject = question_doc['subject']
+  tags = question_doc['tags']
+  query_dict = {
+    'query': {
+      'bool': {
+        'should': [
+          {
+            'match' : {'question' : question}
+          },
+          {
+            'match' : {'tags': {'query':tags, 'boost':100}}
+          }
+        ]
+      }
+    }
+  }
+  if subject:
+    query_dict['query']['bool']['should'].append({'match':{'subject':subject}})
+  return json.dumps(query_dict)
+
 
 # Dictionary that contains all query functions for simple importing.
 query_functions = {
   'default_match_query': default_match_query,
-  'tag_default_query': tag_default_query
+  'tag_match_query_1': tag_match_query_1,
+  'tag_match_query_2': tag_match_query_2,
+  'tag_match_query_3': tag_match_query_3,
+  'tag_match_query_100': tag_match_query_100,
 }
